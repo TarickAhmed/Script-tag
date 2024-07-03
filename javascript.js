0abadf85921a06a80c4a119b5f6014f4ade06fae
Tarick Ahmed <newstartin702@gmail.com>
2024-07-02 21:27:37 -0700

    git commit using terminal

---
+++ javascript.js
@@ -0,0 +1,28 @@
+console.log('Open Class Introduction');
+
+
+/*Question 2: Addition Operation
+
+You have been given two variables, num1 and num2, each with predefined values as test cases. Your task is to calculate the sum of these variables and display the result using the console.log()*/
+var firstName= "Tarick"
+var lastName= "Ahmed"
+var fullName= firstName + " "+ lastName
+console.log(fullName)
+
+
+
+let money = 2;
+let costOfSoda = 4;
+console.log("What is money?", money);
+console.log("What is costOfSoda?", costOfSoda);
+
+
+//if the money is greater than the cost of  the soda, we can buy it
+if(money>=costOfSoda){
+    console.log("I can buy a soda!")
+}
+
+//If we do not have enough money, we cannot buy the soda
+else {
+    console.log("I need to earn some more money!");
+}
